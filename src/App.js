d771e86509c4e51d2414025f8b43aae276497859
tirth2000p <>
2024-01-02 16:12:22 -0500

    updated link

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       setChat([...chat, newChat]);
 
       try {
-          const response = await axios.post('http://localhost:5000/chat', { message: userInput });
+          const response = await axios.post('https://decisive-light-410020.uk.r.appspot.com/chat', { message: userInput });
           setChat([...chat, newChat, { sender: 'Bot', message: response.data.response }]);
       } catch (error) {
           console.error('Error sending message to Flask:', error);
