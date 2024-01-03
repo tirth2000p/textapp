5fd0a8b9acf7166fe875d6ccbfbfc64b086290b8
tirth2000p <80301319+tirth2000p@users.noreply.github.com>
2024-01-03 13:50:48 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       setChat([...chat, newChat]);
 
       try {
-          const response = await axios.post('http://127.0.0.1:5000/chat', { message: userInput });
+          const response = await axios.post('http://127.0.0.1:8000/chat', { message: userInput });
           setChat([...chat, newChat, { sender: 'Bot', message: response.data.response }]);
       } catch (error) {
           console.error('Error sending message to Flask:', error);
